e52c4c3aa2338d2e378f50ed4c00fcfbed4f5e15
RubasznyChomik <poreba.mateusz@gmail.com>
2022-09-26 12:57:40 +0200

    get post

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
-})
+});
 
-ServiceWorkerRegistration.listen(3000, function(){
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+  });
 
-})
-
-
-
-
+  app.post('/', (req, res) => {
+    firstName = req.body.fName;
+    lastName = req.body.lName;
+    email = req.body.email;
+    console.log(firstName, lastName, email);
+  });
+// $('#btn').click(function(){
+//     $.post(__dirname+'/success.html');
+// });
 
 
 
